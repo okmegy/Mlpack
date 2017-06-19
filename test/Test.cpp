ac9abef3c1353f483ed1af42ba5a7432f291ca1a
jasper <okmegy@gotmail.com>
2017-06-19 11:15:30 +0800

    [Mlpack] 1.Revise makefile

--- test/Test.cpp
+++ test/Test.cpp
@@ -9,7 +9,7 @@ using namespace mlpack::metric; // ManhattanDistance
 int main()
 {
 
-
+    string PathName = "/home/jasper/Mlpack/build/iris.csv";
     // example1
     // arma::mat data;
     // data::Load("Path_name", data, true);
@@ -17,16 +17,16 @@ int main()
     // data::Save("cov.csv", cov, true);
 
     //example2
-    arma::mat data;
-    data::Load("/home/jasper/Mlpack/build/iris.csv", data, true);
-    NeighborSearch<NearestNeighborSort, ManhattanDistance> nn(data);
-    arma::Mat<size_t> neighbors;
-    arma::mat distances;
-
-    nn.Search(1, neighbors, distances);
-    for (size_t i = 0; i < neighbors.n_elem; ++i)
-    {
-        std::cout << "Nearest neighbor of point " << i << " is point "
-           << neighbors[i] << " and the distance is " << distances[i] << ".\n";
-     }
+    // arma::mat data;
+    // data::Load(PathName, data, true);
+    // NeighborSearch<NearestNeighborSort, ManhattanDistance> nn(data);
+    // arma::Mat<size_t> neighbors;
+    // arma::mat distances;
+    //
+    // nn.Search(1, neighbors, distances);
+    // for (size_t i = 0; i < neighbors.n_elem; ++i)
+    // {
+    //     std::cout << "Nearest neighbor of point " << i << " is point "
+    //        << neighbors[i] << " and the distance is " << distances[i] << ".\n";
+    //  }
   }
